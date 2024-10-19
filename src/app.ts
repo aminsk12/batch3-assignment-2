0784d549d3c6ec1e13e5c97763b00246bfe8fc1c
amin sk <aminskeh786@gmail.com>
2024-10-19 13:38:29 +0530

    change the foler structure

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
-import express, { Request, Response } from "express";
-import { ProductRoutes } from "./modules/products/product.routes";
-import { orderRoutes } from "./modules/orders/order.routes";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
-const app = express();
+
+const app: Application = express();
 
 //parsers
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/products', ProductRoutes);
-app.use('/api', orderRoutes);
+app.use('/api/products', productsRoute);
+app.use('/api/orders', OrderRoute);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Next.............................!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running');
+});
+
+//global route
+app.all('*', (req: Request, res: Response) => {
+  const id = req.params;
+  res.send(`Sorry man ${id[0]} route is not available`);
 });
 
 export default app;
